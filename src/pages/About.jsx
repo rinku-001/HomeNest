d46e7a7000412fc4b22e53c2eb626976dfbe7cfb
rinku-001 <rinkushakya2360@gmail.com>
2026-07-19 14:45:52 +0530

    Add mission and vision section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,33 @@
+import styles from "./About.module.css";
+
 function About() {
     return (
-        <h1>About Page</h1>
+        <section className={styles.container}>
+
+            <h1 className={styles.heading}>
+                About HomeNest
+            </h1>
+
+            <div className={styles.section}>
+                <h2>Our Mission</h2>
+
+                <p>
+                    Our mission is to provide trusted property listings
+                    and help customers find their dream homes with ease.
+                </p>
+            </div>
+
+            <div className={styles.section}>
+                <h2>Our Vision</h2>
+
+                <p>
+                    Our vision is to become one of the most trusted
+                    real estate platforms by offering quality service
+                    and verified properties.
+                </p>
+            </div>
+
+        </section>
     );
 }
 

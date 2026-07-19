371d9f22a37ba637ac4060754058867a1c0a5426
rinku-001 <rinkushakya2360@gmail.com>
2026-07-19 12:59:32 +0530

    Setup React Router and page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Properties from "./pages/Properties";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
-  
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/properties" element={<Properties />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/Properties.jsx
@@ -0,0 +1,7 @@
+function Properties() {
+    return (
+        <h1>Properties Page</h1>
+    );
+}
+
+export default Properties;
